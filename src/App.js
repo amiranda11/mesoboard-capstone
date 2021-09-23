11c7f58a5a8d9f17f6b50e420028e830afe91701
keviinramiirez <kevin.ramirez3@upr.edu>
2021-09-23 19:34:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useContext } from 'react' //rafce shortcut
+import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Signin } from './content/index';
+import { Layout } from './layout/index'
+import { AuthContext, AuthProvider } from './store/index';
+
+const ProtectedRoute = ({ children, ...rest }) => {
+  const { token } = useContext(AuthContext)
+
+  return (
+    <Route {...rest} render={({ location }) => {
+      console.log('location location', location)
+      return token
+        ? children
+        : <Redirect to={{
+          pathname: '/signin',
+          state: { from: location }
+        }} />
+    }} />
+  )
+}
+
+function App() {
+  // const [testingApiResponse, setApiResponse] = useState('')
+
+  // useEffect(() => {
+  //   const callApi = async () => {
+  //     // await fetch('http://localhost:3001/test', {
+  //     //   methoid: 'GET',
+  //     //   headers: {
+  //     //     'Content-Type': 'application/text',
+  //     //     Authorization: `Bearer ${REACT_APP_TOKEN}`
+  //     //   }
+  //     // })
+  //     await fetch('http://localhost:3001/test')
+  //       .then(res => res.text())
+  //       .then(res => setApiResponse(res));
+  //   }
+  //   callApi()
+  // }, [])
+  return (
+    <AuthProvider>
+      <Router>
+        <Route exact path='/'><Redirect to='/signin' /></Route>
+        <Route path='/signin' component={Signin} />
+        <ProtectedRoute path='/app/:view'>
+          <Layout />
+        </ProtectedRoute>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/helpers/url.js
@@ -0,0 +1,14 @@
+export const pathHome = '/app/home'
+export const pathSchedule = '/app/schedule'
+export const pathProfiles = '/app/profiles'
+export const pathRequests = '/app/requests'
+export const paths = {
+  // transfer above to here please. For future use
+}
+
+/** returns the slug of the given url. */
+export const urlSlug = url => url.substring(url.lastIndexOf('/') + 1)
+
+/** returns the given url without the slug. */
+export const domainUrl = url => url.substring(0, url.lastIndexOf('/'))
+
